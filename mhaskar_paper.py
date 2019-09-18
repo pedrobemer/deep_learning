6ac36241fe7433800000993b0ce633d31d830a8a
PedroBemerguy <pedrobemerguy96@gmail.com>
2019-09-18 17:27:07 -0300

    Update Mashkar paper implementation

--- mhaskar_paper.py
+++ mhaskar_paper.py
@@ -1,23 +1,18 @@
 # To split the dataset into train and test datasets
 from math import cos,pi
 from sklearn.model_selection import train_test_split
-
-# TensorFlow e tf.keras
-import tensorflow as tf
-from tensorflow import keras
-
 from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, BatchNormalization, Input
-from keras.wrappers.scikit_learn import KerasRegressor
+from keras.layers import Dense, BatchNormalization
 from keras import optimizers
 
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
 
 def create_nn(num_hidden_layers, neurons):
-
     activation = 'relu'
     model = Sequential()
+    print("Hidden Layers: ", num_hidden_layers, "Neurons per Hidden Layer: ", neurons)
     if (num_hidden_layers > 3):
         return -1
 
@@ -36,36 +31,40 @@ def create_nn(num_hidden_layers, neurons):
 
 def main():
     hidden_layers = [1,2,3]
-    for h in hidden_layers:
-        #Equation
-        y = lambda x : 2*((2*(cos(x)**2)-1)**2)
 
-        #Uniform samples for x in the above equation
-        total_samples = int(120e3)
-        x_samples = np.random.uniform(-2*pi,2*pi,total_samples)
-        x_features_train = x_samples[0:int(total_samples/2)]
-        y_label_train = list(map(y, x_features_train))
-        x_features_test = x_samples[int(total_samples/2):int(total_samples)]
-        y_label_test = list(map(y, x_features_test))
-        
+    with open('mhasker_paper_results.csv', 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',')
+        spamwriter.writerow(['MSE', 'Total Parameters', 'Total Units', 'Hidden Layers'])
+        for h in hidden_layers:
+            #Equation
+            y = lambda x : 2*((2*(cos(x)**2)-1)**2)
 
-        if (h == 1):
-            neurons = [24,48,72,128,256]
-        elif (h == 2): 
-            neurons = [12,24,36]
-        elif (h == 3):
-            neurons = [8,16,24]
-        for j in range(len(neurons)):
-            model = create_nn(h,neurons[j])
-            if (model == -1):
-                print("Configuration Error")
-            else:
-                model.summary()
-                sgd = optimizers.SGD(lr=0.0001, momentum=0.9)
-                model.compile(loss='mean_squared_error', optimizer=sgd, metrics=["mse"])
-                model.fit(x_features_train, y_label_train, epochs=2000, batch_size=3000)
-                loss_and_metrics = model.evaluate(x_features_test, y_label_test)
-                print(loss_and_metrics)
+            #Uniform samples for x in the above equation
+            total_samples = int(120e3)
+            x_samples = np.random.uniform(-2*pi,2*pi,total_samples)
+            x_features_train = x_samples[0:int(total_samples/2)]
+            y_label_train = list(map(y, x_features_train))
+            x_features_test = x_samples[int(total_samples/2):int(total_samples)]
+            y_label_test = list(map(y, x_features_test))
+
+            if (h == 1):
+                neurons = [24,48,72,128,256]
+            elif (h == 2):
+                neurons = [12,24,36]
+            elif (h == 3):
+                neurons = [8,16,24]
+            for j in range(len(neurons)):
+                model = create_nn(h,neurons[j])
+                print(model.count_params())
+                if (model == -1):
+                    print("Configuration Error")
+                else:
+                    model.summary()
+                    sgd = optimizers.SGD(lr=0.0001, momentum=0.9)
+                    model.compile(loss='mean_squared_error', optimizer=sgd)
+                    model.fit(x_features_train, y_label_train, epochs=2000, batch_size=3000)
+                    loss = model.evaluate(x_features_test, y_label_test)
+                    spamwriter.writerow([loss, model.count_params(), h*neurons[j]],h)
 
 
 main()
